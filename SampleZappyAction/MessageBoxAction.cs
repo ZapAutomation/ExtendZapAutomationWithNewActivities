c67674226ee3588e66b80033f87e1e2b85c62857
swaraj <swaraj@aniroj.com>
2019-06-11 13:54:20 +0000

    Updated message box action

--- SampleZappyAction/MessageBoxAction.cs
+++ SampleZappyAction/MessageBoxAction.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Zappy.SharedInterface;
+using Zappy.SharedInterface.Helper;
 
 namespace Zappy.SampleAction
 {
-    class MessageBoxAction
+    public class MessageBoxAction : TemplateAction
     {
+        [Category("Input")]
+        [Description("InputBox Text")]
+        public DynamicProperty<string> Text { get; set; }
+
+        public override void Invoke(IZappyExecutionContext context, ZappyTaskActionInvoker actionInvoker)
+        {
+            MessageBox.Show(Text);
+        }
+
+
+        public override string AuditInfo()
+        {
+            return base.AuditInfo() + " Text:" + this.Text;
+        }
     }
 }
